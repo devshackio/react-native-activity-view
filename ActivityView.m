acbcb448c18753bec697e4e69a713c3c4475850e
Joe Noon <joenoon@gmail.com>
2016-06-23 21:05:48 -0700

    add extra string param named hashtag

--- ActivityView.m
+++ ActivityView.m
@@ -99,6 +99,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args)
 {
     NSMutableArray *shareObject = [NSMutableArray array];
     NSString *text = args[@"text"];
+    NSString *hashtag = args[@"hashtag"];
     NSURL *url = args[@"url"];
     NSObject *file = args[@"file"];
     NSArray *activitiesToExclude = args[@"exclude"];
@@ -111,6 +112,10 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args)
 
     if (text) {
         [shareObject addObject:text];
+    }
+
+    if (hashtag) {
+        [shareObject addObject:hashtag];
     }
 
     if (url) {
