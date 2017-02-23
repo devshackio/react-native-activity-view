6af68bea1319df1f11597511b642e2d61961c217
Matt Jeanes <MattBlooJeans@gmail.com>
2017-02-23 10:10:22 -0500

    Fix android stub

--- ActivityView.android.js
+++ ActivityView.android.js
@@ -6,12 +6,7 @@
  */
 'use strict';
 
-var warning = require('warning');
-
 var ActivityView = {
-  test: function test() {
-    warning('Not yet implemented for Android.');
-  }
 };
 
 module.exports = ActivityView;
